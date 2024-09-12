3285659d8993a306979228a5db3d829d37cf1a73
LordSchocki <lordschockimail@gmail.com>
2024-09-12 18:46:08 +0200

    init commit

---
+++ src/main/java/de/ventilover/funnyStuff/FunnyStuff.java
@@ -0,0 +1,17 @@
+package de.ventilover.funnyStuff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FunnyStuff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
